bf7eb23f507e56ae87507a2c75b2632a4beb904c
Josh Nicholas <hi@thambi.li>
2026-06-30 20:18:44 +1000

    start

---
+++ threejs-journey/lessons/03/exercise/script.js
@@ -0,0 +1,36 @@
+import * as THREE from "three"
+
+const canvas = document.querySelector('canvas.webgl')
+console.log("canvas: ", canvas)
+const scene = new THREE.Scene()
+console.log('scene: ', scene)
+// const myGeometry = new THREE.SphereGeometry(1.5,32,32)
+
+// Object
+const geometry = new THREE.BoxGeometry(1, 1, 1)
+const material = new THREE.MeshBasicMaterial({color: 'red'})
+const mesh = new THREE.Mesh(geometry, material)
+
+scene.add(mesh)
+
+/// Camera
+const sizes = {
+    width: 800,
+    height: 600
+}
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
+camera.position.z = 3
+scene.add(camera)
+
+
+// Renderer
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+
+renderer.render(scene, camera)
+
+console.log("Javascript is working")
+// console.log(THREE)
