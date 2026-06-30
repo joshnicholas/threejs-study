2da7da8442bc87ef4a11bdb2c82407ee95b1fabe
Josh Nicholas <hi@thambi.li>
2026-07-01 08:03:23 +1000

    new

--- threejs-journey/lessons/04/starter/src/script.js
+++ threejs-journey/lessons/04/starter/src/script.js
@@ -9,10 +9,57 @@ const scene = new THREE.Scene()
 /**
  * Objects
  */
-const geometry = new THREE.BoxGeometry(1, 1, 1)
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
-const mesh = new THREE.Mesh(geometry, material)
-scene.add(mesh)
+// const geometry = new THREE.BoxGeometry(1, 1, 1)
+// const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+// const mesh = new THREE.Mesh(geometry, material)
+
+// mesh.position.x = 0.7
+// mesh.position.y = -0.6
+// mesh.position.z = 1
+// scene.add(mesh)
+
+
+// Scale
+// mesh.scale.x = 2
+// mesh.scale.y = 0.5
+// mesh.scale.z = 0.5
+// mesh.scale.set(2, 0.5, 0.5)
+
+// mesh.rotation.reorder('YXZ')
+// mesh.rotation.y = Math.PI * 0.25
+// mesh.rotation.x = Math.PI * 0.25
+// mesh.rotation.z = 0
+
+const group = new THREE.Group()
+group.position.y = 1
+group.scale.y = 2
+group.rotation.y = 1
+scene.add(group)
+
+const cube1 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: 'red'})
+)
+group.add(cube1)
+
+
+const cube2 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: 'green'})
+)
+cube2.position.x = -2
+group.add(cube2)
+
+const cube3 = new THREE.Mesh(
+    new THREE.BoxGeometry(1, 1, 1), 
+    new THREE.MeshBasicMaterial({color: 'blue'})
+)
+cube3.position.x = 2
+group.add(cube3)
+
+
+const axesHelper = new THREE.AxesHelper()
+scene.add(axesHelper)
 
 /**
  * Sizes
@@ -27,9 +74,15 @@ const sizes = {
  */
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height)
 camera.position.z = 3
+// camera.position.y = 1
+// camera.position.x = 1
 scene.add(camera)
 
+// camera.lookAt(mesh.position)
+
+// console.log(mesh.position.distanceTo(camera.position))
 /**
+ * 
  * Renderer
  */
 const renderer = new THREE.WebGLRenderer({
